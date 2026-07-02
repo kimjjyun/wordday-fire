61f9ebcfe624fcbf558558eb257c38acafc431c4
kimjieon <kimjjyun7@gamil.com>
2026-07-02 17:22:34 +0900

    Fix iPhone PWA header safe area

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
-import { isStandaloneDisplayMode } from './utils/displayMode';
 
-if (isStandaloneDisplayMode()) {
-  document.documentElement.classList.add('wordday-standalone');
-}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
